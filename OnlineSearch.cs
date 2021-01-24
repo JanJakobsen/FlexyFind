8b1498a3ab7b9fdb381bb650c9c6cf2d98a00c4b
Jan Jakobsen <janjakob69@gmail.com>
2021-01-24 21:57:42 +0100

    Get Bing subscription key from Azure key vault

--- OnlineSearch.cs
+++ OnlineSearch.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Net;
 using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Azure.KeyVault;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using Newtonsoft.Json;
 
 namespace OnlineSearchHelper
@@ -12,22 +14,21 @@ namespace OnlineSearchHelper
     /// </summary>
     static class OnlineSearcher
     {
-        private const string bingKey = "ff5eae65dd8542c6809a4e17a97be178";  // TODO: Put this key in an Azure key vault
-        private const string bingEndpoint = "https://api.bing.microsoft.com/v7.0/search";
-
         /// <summary>
         /// Do a web search for a certain criteria and a maximal number of hits
         /// </summary>
         public static List<string> Search(string qry, int maxhits=5)
         {
+            const string bingEndpoint = "https://api.bing.microsoft.com/v7.0/search";
+
             try
             {
                 // Construct the URI including max number of hits
                 var uriQuery = bingEndpoint + "?q=" + Uri.EscapeDataString(qry) + "&count=" + maxhits.ToString();
 
-                // Perform request
+                // Perform request with subscription key from Azure key vault
                 WebRequest request = HttpWebRequest.Create(uriQuery);
-                request.Headers["Ocp-Apim-Subscription-Key"] = bingKey;
+                request.Headers["Ocp-Apim-Subscription-Key"] = GetKeyfromVault();
 
                 // Get response and read it as a json file
                 HttpWebResponse response = (HttpWebResponse)request.GetResponseAsync().Result;
@@ -60,6 +61,35 @@ namespace OnlineSearchHelper
             }
 
             return result;
+        }
+
+        /// <summary>
+        /// Get Bing Search key from my Azure key vault
+        /// </summary>
+        private static string GetKeyfromVault()
+        {
+            const string keyVault = "https://janskeyvault.vault.azure.net/";
+            const string secretName = "BingSearch";
+
+            KeyVaultClient client = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(GetKeyVaultToken));
+            var secret = client.GetSecretAsync(keyVault, secretName).GetAwaiter().GetResult();
+
+            return secret.Value;
+        }
+
+        /// <summary>
+        /// Give this client access to the key vault
+        /// </summary>
+        static async Task<string> GetKeyVaultToken(string authority, string resource, string scope)
+        {
+            const string clientId = "ddd88754-e541-48af-85bc-11778a7cc5f3";
+            const string clientSecret = "2qdX.Jm-5w11_2Go5_I_Ex8tq5WMs.0m_~";
+
+            ClientCredential credential = new ClientCredential(clientId, clientSecret);
+            var context = new AuthenticationContext(authority, TokenCache.DefaultShared);
+            var result = await context.AcquireTokenAsync(resource, credential);
+
+            return result.AccessToken;
         }
     }
 }
